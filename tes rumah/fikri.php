8b8267a5539fa58fbf488d28aad5d693fc830c59
Ihwanulfikriramadhan7 <118697086+Ihwanulfikriramadhan7@users.noreply.github.com>
2023-03-04 21:12:49 +0700

    Pertemuan 2
    Pertemuan 2

---
+++ tes rumah/fikri.php
@@ -0,0 +1,27 @@
+<?php 
+//awalan belajar sintak php
+    /*standar uotput dalam php
+    1. echo, print
+    2. print_r (untuk print dalam array)
+    3. var_dump(di ginakan untuk melihat isi vaiabel)
+    2-3 biasa di bagi untuk melihat kesalahan dimana dan ingin melihat isi dalam variabel
+     */
+
+     //penlisan sintakphp
+    ?> 
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+         <meta charset="UTF-8">
+         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <title>Document</title>
+     </head>
+     <body>
+         <h1>Selamat datang <?php echo "ihwanul";?></h1>
+         <?php
+         echo"<h1>fikri</h1>"
+         ?>
+     </body>
+     </html>
+
