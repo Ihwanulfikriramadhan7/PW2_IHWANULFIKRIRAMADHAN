660d97460a41ab898bf4a3c8f35a7c28c66e62c0
Ihwanulfikriramadhan7 <118697086+Ihwanulfikriramadhan7@users.noreply.github.com>
2023-03-04 21:13:59 +0700

    Delete halaman.php

--- tes rumah/halaman.php
+++
@@ -1,9 +0,0 @@
-<?php 
- $_nama = $_GET['mesjid'];
- $_kecamatan = $_GET['kecamatan'];
- $_deskripsi = $_GET['text'];
-
- echo 'Nama mesjid : '. $_nama;
- echo '<br>Kecamatan : '. $_kecamatan;
- echo '<br>deskripsi : '. $_deskripsi;
-?>
