83f8c91d81e575024e6d31313f76bfdbd9581806
Ihwanulfikriramadhan7 <118697086+Ihwanulfikriramadhan7@users.noreply.github.com>
2023-05-16 19:57:44 +0700

    Pertemuan 8
    Pertemuan 8

---
+++ storage/framework/views/91309b09a7aa288fb0900c1a78b5569b.php
@@ -0,0 +1,2 @@
+<h2>halaman</h2>
+Kategori User: <?= $kategoriuser; ?><?php /**PATH C:\Users\user\Desktop\belajar-laravel\resources\views/user/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bf9d4606c94c48f01d50842432143e0c.php
@@ -0,0 +1,3 @@
+<h2>halaman data produk</h2>
+
+ID : <?= $id; ?><?php /**PATH C:\Users\user\Desktop\belajar-laravel\resources\views/produk/index.blade.php ENDPATH**/ ?>
